a1d0ec6249408d39cc5564ca10eeafe20327ebf6
Sahil Ashar <sahilashar16@gmail.com>
2022-12-19 22:28:26 -0800

    did some rust book chapter 12

--- 2022/Day1/src/main.rs
+++ 2022/Day1/src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+
+use day1::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.query);
+
+    if let Err(e) = day1::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
